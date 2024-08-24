5a57c3874df2d688778a5d25d2d8053c5761db68
Daniel Mendoza <74325093+DEM2@users.noreply.github.com>
2024-08-23 23:12:17 -0500

    desarrollo del punto 4

--- taller2.js
+++ taller2.js
@@ -1,26 +1,37 @@
 //--punto 1---
-function findMax(array){
-    let max=0
-    for(let i=0;i<array.length;i++){
-        if (array[i]> max) max=array[i]
-    }
-    return max
+function findMax(array) {
+  let max = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > max) max = array[i];
+  }
+  return max;
 }
 
 //--punto 2---
-function includes(array, data){
-    for(let i=0;i<array.length;i++){
-        if (array[i]===data) return true
-    }
-    return false
+function includes(array, data) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === data) return true;
+  }
+  return false;
 }
 
 //--punto 3---
-function sum(array){
-  return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+function sum(array) {
+  return array.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
 }
 
 //--punto 4---
-function missingNumbers(array){
-  
+function missingNumbers(array) {
+  array.sort((a, b) => a - b);
+  let faltantes = [];
+  for (let i = array[0]; i <= array[array.length - 1]; i++) {
+    if (!array.includes(i)) {
+      faltantes.push(i);
+    }
+  }
+
+  return faltantes;
 }
